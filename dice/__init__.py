5d0ca0c251d0bb40afd3534a64187f69807c5e3f
szepepjuice <cohomolotopolo@qq.com>
2021-08-09 00:18:58 +0800

    [feat]: 骰子计算器

--- dice/__init__.py
+++ dice/__init__.py
@@ -1,1 +1,23 @@
+'''
+骰子/计算器工具
+d100: 100面骰
+2d100: 两个100面骰相加
+2d100s: 两个100面骰里较小的那个
+3d100s2: 三个100面骰里小的两个之和
+3d100l2： 三个100面骰里大的两个之和
+dp, db: CoC 7th惩罚骰、奖励骰
+3dp: 等价于dp+dp+dp
+9999999d9999999: 没事，骰子够多
+d6*(d6+d6): 字面意思
+d6!: d6的阶乘
+
+
+可用运算符（含义同Python）：
++,-,*,/,%,//,**
+可用函数：
+exp,log,log10,sin,cos,abs,sqrt,ceil,floor,round,max,min
+可用常量：
+pi
+'''
+
 from .dice_roller import parse_and_eval_dice
--- dice/dice_roller.py
+++ dice/dice_roller.py
@@ -1,28 +1,6 @@
 # Dice parser using Pratt parsing
 # see https://matklad.github.io/2020/04/13/simple-but-powerful-pratt-parsing.html
 
-'''
-骰子/计算器工具
-d100: 100面骰
-2d100: 两个100面骰想加
-2d100s: 两个100面骰里较小的那个
-3d100s2: 三个100面骰里小的两个之和
-3d100l2： 三个100面骰里大的两个之和
-dp, db: CoC 7th惩罚骰、奖励骰
-3dp: 等价于dp+dp+dp
-9999999d9999999: 没事，骰子够多
-d6*(d6+d6): 字面意思
-d6!: d6的阶乘
-
-
-可用运算符（含义同Python）：
-+,-,*,/,%,//,**
-可用函数：
-exp,log,log10,sin,cos,abs,sqrt,ceil,floor,round,max,min
-可用常量：
-pi
-'''
-
 
 ND_CAP = 100 # 超过这个值的单次掷骰采用快速算法，或者直接拒绝计算了
 
